48c4de6cafdc43bd756cfe213a189119a69c8e58
Brent Longborough <brent+ctancontrib@longborough.org>
2014-12-06 18:48:33 +0000

    Reformat tests and build configs

--- build.lua
+++ build.lua
@@ -1,13 +1,16 @@
 #!/usr/bin/env texlua
 -- Build script for gitinfo2
 module = "gitinfo2"
-    sourcefiles     = { "*.sty" }
-    typesetfiles    = { "gitinfo2.tex" }
-    textfiles       = { "gitPseudoHeadInfo.gin", "post-xxx-sample.txt" }
-    demofiles       = { "gitinfotest.tex" }
-    readmefiles     = { "README" }
-    typesetexe      = "xelatex"
-    packtdszip      = true
-    typesetopts     = "-interaction=batchmode"
+  sourcefiles  = {"*.sty"}
+  typesetfiles = {"gitinfo2.tex"}
+  textfiles    = {
+                  "gitPseudoHeadInfo.gin",
+                  "post-xxx-sample.txt",
+                 }
+  demofiles    = {"gitinfotest.tex"}
+  readmefiles  = {"README"}
+  typesetexe   = "xelatex"
+  packtdszip   = true
+  typesetopts  = "-interaction=batchmode"
 kpse.set_program_name("kpsewhich")
 dofile(kpse.lookup("l3build.lua"))
--- tub.lua
+++ tub.lua
@@ -2,10 +2,10 @@
 -- Build script for gitinfo2
 module = "gitinfo2"
 lvtext = ".lll"
-    installfiles    = { "*.sty", } 
-    sourcefiles     = { "*.sty", }
-    textfiles       = { "post-xxx-sample.txt", }
-    readmefiles     = { "README", }
-    packtdszip      = true
+  installfiles = {"*.sty"} 
+  sourcefiles  = {"*.sty"}
+  textfiles    = {"post-xxx-sample.txt"}
+  readmefiles  = {"README"}
+  packtdszip   = true
 kpse.set_program_name("kpsewhich")
 dofile(kpse.lookup("l3build.lua"))
