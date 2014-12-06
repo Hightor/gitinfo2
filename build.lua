635ffd54b1fca6e2d4b8df2d963c9fb6761a4482
Brent Longborough <brent+ctancontrib@longborough.org>
2014-12-06 11:51:56 +0000

    Final build configuration after fixes to l3build

--- build.lua
+++ build.lua
@@ -1,22 +1,13 @@
 #!/usr/bin/env texlua
 -- Build script for gitinfo2
 module = "gitinfo2"
-
-    installfiles    = { "*.sty", } 
-    sourcefiles     = { 
-                        "gitinfo2.tex", 
-                        "*.sty", 
-                        }
-    typesetfiles    = { "gitinfo2.tex", }
-    textfiles       = { 
-                        "gitPseudoHeadInfo.gin", 
-                        "post-xxx-sample.txt", 
-                        }
-    demofiles       = { "gitinfotest.tex", }
-    readmefiles     = { "README", }
+    sourcefiles     = { "*.sty" }
+    typesetfiles    = { "gitinfo2.tex" }
+    textfiles       = { "gitPseudoHeadInfo.gin", "post-xxx-sample.txt" }
+    demofiles       = { "gitinfotest.tex" }
+    readmefiles     = { "README" }
     typesetexe      = "xelatex"
     packtdszip      = true
     typesetopts     = "-interaction=batchmode"
-
 kpse.set_program_name("kpsewhich")
 dofile(kpse.lookup("l3build.lua"))
